07840c895eb77e108ebd691f43d36c4d61650232
lululeon <thelululeon@gmail.com>
2021-08-27 21:21:39 -0400

    finalize node ci pipeline checks

--- jest.config.js
+++ jest.config.js
@@ -6,12 +6,16 @@ const rootDir = __dirname
 
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = config = {
+  // see: https://kulshekhar.github.io/ts-jest/docs/getting-started/presets
   preset: 'ts-jest',
+
   testEnvironment: 'node',
   roots: [`${rootDir}/src`],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+
+  // allows jest to determine correct paths to @/-prefixed imports, to feed in turn to ts-jest (which does zero path resolution):
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: `${rootDir}/src`,
   }),
